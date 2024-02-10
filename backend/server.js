438e2a3afd16265ebf2a50f283db596dcd11f749
pratik-win-PC <pratiktravelogue@gmail.com>
2024-02-10 10:30:00 +0530

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db/index.js";
 
 //Route Imports
 import authRoutes from "./routes/auth.routes.js";
@@ -10,9 +11,17 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get("/",(req,res) => {
-  res.send("HEllo World")
+  res.send("HEllo World") 
 });
 
 app.use("/api/auth",authRoutes);
 
-app.listen(PORT,() => console.log(`server is running on port ${PORT}`));
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000 , () => {
+    console.log(`Server is running at port ${process.env.PORT}`)
+  })
+})
+.catch((err) => {
+  console.log("MongoDB Conntection has failed due to ", err);
+})
