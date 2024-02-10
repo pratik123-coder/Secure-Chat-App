6102dd123fb9aaa2d64409aa19438996c1be401d
pratik-win-PC <pratiktravelogue@gmail.com>
2024-02-10 07:45:12 +0530

    Auth Routes and Controllers Updated

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const login = (req,res) => {
+  console.log("Login User");
+}
+
+export const logout = (req,res) => {
+  console.log("Logout User");
+}
+
+export const signup = async (req,res) => {
+  try {
+    const{ fulName, userName, password, confirmPassword } = req.body;
+  }
+  } catch (error) {
+    
+  }
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+//Route Imports
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -7,5 +12,7 @@ const PORT = process.env.PORT || 8000;
 app.get("/",(req,res) => {
   res.send("HEllo World")
 });
+
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT,() => console.log(`server is running on port ${PORT}`));
