e83d3830ebf3dfe03f42ec336be4be55c387019d
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-11 13:31:44 +0530

    Message Conversation between Two users Done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,36 @@
+import Conversation from "../models/converstation.model.js";
+import { Message } from "../models/message.model.js";
+
 export const sendMessage = async ( req, res ) => {
     try {
         const { message} = req.body;
-        const {id} = req.params.id;
-        const senderId = req.userId;      
+        const {id:recieverId} = req.params;
+        const senderId = req.user._id;  
+        let converstation = await Conversation.findOne({
+            participants:{
+                $all: [senderId, recieverId]
+            }
+        })  
+        if(!converstation) {
+            converstation = await Conversation.create({
+                participants: [senderId, recieverId]
+            })
+        }
+        const newMessage = await Message.create({
+            senderId,
+            recieverId,
+            message
+        })
+
+        if(newMessage){
+            converstation.messages.push(newMessage._id);
+
+        }
+        //Socket Adding Will Go Here
+
+        
+        await Promise.all([conversation.save(),newMessage.save()]);
+        res.status(201).json(newMessage);
     } catch (error) {
         console.log("Error in Sending Message",error.message);
         res.status(500).json({ error: "Internal Server Error" });
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
 
 //Route Imports
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 8000;
 
 
 app.use(express.json());  //to parse the incoming requests from JSON payloads (from req.body)
+app.use(cookieParser()); //middleware for cookie
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
