5405524cfc560684b6a6d82c0759572cf7a63557
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-17 22:51:34 +0530

    frontend changes

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,19 +1,34 @@
-const GenderRadio = () => {
+import React from "react";
+
+const GenderRadio = ({ onCheckboxChange, selectedGender }) => {
     return (
         <div className='flex'>
             <div className='form-control'>
                 <label className={`label gap-2 cursor-pointer`}>
                     <span className='label-text'>Male</span>
-                    <input type='radio' name='gender' className='radio border-slate-900' />
+                    <input
+                        type='radio'
+                        name='gender'
+                        className='radio border-slate-900'
+                        checked={selectedGender === 'male'}
+                        onChange={() => onCheckboxChange('male')}
+                    />
                 </label>
             </div>
             <div className='form-control'>
                 <label className={`label gap-2 cursor-pointer`}>
                     <span className='label-text'>Female</span>
-                    <input type='radio' name='gender' className='radio border-slate-900' />
+                    <input
+                        type='radio'
+                        name='gender'
+                        className='radio border-slate-900'
+                        checked={selectedGender === 'female'}
+                        onChange={() => onCheckboxChange('female')}
+                    />
                 </label>
             </div>
         </div>
     );
 };
+
 export default GenderRadio;
