e2e79c19100aa31d9e94625552bea1fcc2d55ef1
pratik-win-PC <pratiktravelogue@gmail.com>
2024-03-21 10:13:18 +0530

    Diffie Hellman Key Exchange Added in Socket.io

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,67 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import crypto from "crypto";
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: ["http://localhost:3000"],
-		methods: ["GET", "POST"],
-	},
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+    },
 });
 
+const userSocketMap = {}; // {userId: socketId}
 export const getReceiverSocketId = (receiverId) => {
 	return userSocketMap[receiverId];
 };
 
-const userSocketMap = {}; // {userId: socketId}
+
+// Function to generate Diffie-Hellman key pair
+function generateDHKeyPair() {
+	const dh = crypto.createDiffieHellman(2048); // Increase the key size to 2048 bits
+	dh.generateKeys();
+	return {
+			prime: dh.getPrime('hex'),
+			generator: dh.getGenerator('hex'),
+			publicKey: dh.getPublicKey('hex'),
+			privateKey: dh.getPrivateKey('hex')
+	};
+}
+// Function to compute shared secret from own private key and other party's public key
+function computeSharedSecret(myPrivateKey, otherPublicKey) {
+    const dh = crypto.createDiffieHellman(Buffer.from(myPrivateKey, 'hex'));
+    return dh.computeSecret(Buffer.from(otherPublicKey, 'hex')).toString('hex');
+}
 
 io.on("connection", (socket) => {
-	console.log("a user connected", socket.id);
+    console.log("a user connected", socket.id);
 
-	const userId = socket.handshake.query.userId;
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+    const userId = socket.handshake.query.userId;
+    if (userId !== "undefined") userSocketMap[userId] = socket.id;
 
-	// io.emit() is used to send events to all the connected clients
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    // Generate DH key pair for this connection
+    const dhKeys = generateDHKeyPair();
+    socket.emit("dhKeys", dhKeys);
 
-	// socket.on() is used to listen to the events. can be used both on client and server side
-	socket.on("disconnect", () => {
-		console.log("user disconnected", socket.id);
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+    socket.on("otherPublicKey", (otherPublicKey) => {
+        // Compute shared secret
+        const sharedSecret = computeSharedSecret(dhKeys.privateKey, otherPublicKey);
+        console.log("Shared secret:", sharedSecret);
+
+    });
+
+    // io.emit() is used to send events to all the connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    // socket.on() is used to listen to the events. can be used both on client and server side
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
 });
 
 export { app, io, server };
--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -6,18 +6,27 @@ import useConversation from "../zustand/useConversation";
 import notificationSound from "../assets/sounds/notification.mp3";
 
 const useListenMessages = () => {
-	const { socket } = useSocketContext();
-	const { messages, setMessages } = useConversation();
+    const { socket } = useSocketContext();
+    const { messages, setMessages } = useConversation();
 
-	useEffect(() => {
-		socket?.on("newMessage", (newMessage) => {
+    useEffect(() => {
+        socket?.on("newMessage", (newMessage) => {
+            const sound = new Audio(notificationSound);
+            sound.play();
+            setMessages([...messages, newMessage]);
+        });
 
-			const sound = new Audio(notificationSound);
-			sound.play();
-			setMessages([...messages, newMessage]);
-		});
+        // Listen for 'dhKeys' event
+        socket?.on("dhKeys", (dhKeys) => {
+            // Handle the received DH keys, you might want to store them or use them for encryption
+            console.log("Received DH keys:", dhKeys);
+        });
 
-		return () => socket?.off("newMessage");
-	}, [socket, setMessages, messages]);
+        return () => {
+            socket?.off("newMessage");
+            socket?.off("dhKeys"); // Make sure to remove the listener when unmounting
+        };
+    }, [socket, setMessages, messages]);
 };
+
 export default useListenMessages;
