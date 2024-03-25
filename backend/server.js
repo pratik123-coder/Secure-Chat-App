f6e3933e495d456f0c6b73b9c6e5318778c18cdf
pratik-win-PC <pratiktravelogue@gmail.com>
2024-03-25 09:37:21 +0530

    packages updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import connectDB from "./db/index.js";
+import path from "path";
 
 //Route Imports
 import authRoutes from "./routes/auth.routes.js";
@@ -12,6 +13,8 @@ import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 
 app.use(express.json());  //to parse the incoming requests from JSON payloads (from req.body)
 app.use(cookieParser()); //middleware for cookie
@@ -19,6 +22,12 @@ app.use(cookieParser()); //middleware for cookie
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+});
 
 connectDB()
 .then(() => {
