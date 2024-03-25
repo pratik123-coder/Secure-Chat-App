769150158ebdc1442ed139455f85c5b57eb3c386
pratik-win-PC <pratiktravelogue@gmail.com>
2024-03-25 11:07:36 +0530

    socket fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy:{
       "/api": {
-        target: 'https://secure-chat-app-3hiy.onrender.com',
+        target: 'http://localhost:8000',
       }
     }
   },
